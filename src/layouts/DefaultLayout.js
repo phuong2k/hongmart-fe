59f85654cc40169fa2e68c680b298904e6539ab5
phuong <phuong.kizaru@gmail.com>
2023-03-13 20:59:18 +0700

    1/source

---
+++ src/layouts/DefaultLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DefaultLayout(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default DefaultLayout;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DefaultLayout from '../layouts/DefaultLayout';
+
+function HomePage(props) {
+  return (
+    <div>
+        <DefaultLayout>
+        </DefaultLayout>
+    </div>
+  );
+}
+
+export default HomePage;
